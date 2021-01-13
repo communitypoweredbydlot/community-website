8338e5d93a1c8f4ebcdcbdbf75b96b4c49483ac3
Csernik Flaviu Andrei <fcsernik@gmail.com>
2021-01-13 17:59:37 +0200

    Auto-capitalize only the first word in page titles, fix #34

--- components/PageTitle.vue
+++ components/PageTitle.vue
@@ -1,6 +1,6 @@
 <template>
-  <h3 class="page-title-sm xl:page-title capitalize">
-    {{ $t(path) }}
+  <h3 class="page-title-sm xl:page-title">
+    {{ capitalizeFirst($t(path)) }}
   </h3>
 </template>
 
@@ -10,5 +10,9 @@ import { Component, Prop, Vue } from 'nuxt-property-decorator'
 @Component
 export default class PageTitle extends Vue {
   @Prop() path!: string
+
+  capitalizeFirst (sentence: string): string {
+    return sentence.charAt(0).toUpperCase() + sentence.slice(1)
+  }
 }
 </script>
