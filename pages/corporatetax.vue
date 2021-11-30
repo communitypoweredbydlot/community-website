e4294ebe2882165d87d864c78ffe14365fc0550a
Szocs, Dan <dan.szocs@yahoo.com>
2021-11-30 20:48:31 +0200

    define corporate tax page, #105

---
+++ pages/corporatetax.vue
@@ -0,0 +1,24 @@
+<template>
+  <article class="flex flex-col gap-6">
+    <div>
+      <page-title path="corporatetax.title" />
+    </div>
+  </article>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+import ogMetaFor from '@/lib/HeaderMeta'
+
+@Component
+export default class CorporateTax extends Vue {
+  head () {
+    return ogMetaFor({
+      route: this.$nuxt.$route.path,
+      img: '/og_corporatetax.jpg',
+      title: this.$t('corporatetax.title')
+    })
+  }
+}
+</script>
